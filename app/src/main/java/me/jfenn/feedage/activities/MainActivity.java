cfbb68eea1e3b8a12eab6a8a602b742714aeedf9
James Fenn <18jafenn90@gmail.com>
2018-05-29 09:41:31 -0400

    basic about screen

--- app/src/main/java/me/jfenn/feedage/activities/MainActivity.java
+++ app/src/main/java/me/jfenn/feedage/activities/MainActivity.java
@@ -3,9 +3,13 @@ package me.jfenn.feedage.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
+import me.jfenn.attribouter.Attribouter;
 import me.jfenn.feedage.Feedage;
 import me.jfenn.feedage.R;
 import me.jfenn.feedage.fragments.BaseFragment;
@@ -42,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements FeedageLib.OnCate
 
         getSupportFragmentManager().addOnBackStackChangedListener(this);
         feedage.addListener(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -63,5 +70,21 @@ public class MainActivity extends AppCompatActivity implements FeedageLib.OnCate
     @Override
     public void onBackStackChanged() {
         fragment = (BaseFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+
+        } else if (item.getItemId() == R.id.action_about)
+            Attribouter.from(this).show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
