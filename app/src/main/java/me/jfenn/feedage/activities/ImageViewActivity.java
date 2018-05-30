2b7d4f9894a5b18b4c991eea3e6bc51983a50e57
James Fenn <18jafenn90@gmail.com>
2018-05-30 17:26:09 -0400

    fixed exit image transition

--- app/src/main/java/me/jfenn/feedage/activities/ImageViewActivity.java
+++ app/src/main/java/me/jfenn/feedage/activities/ImageViewActivity.java
@@ -39,7 +39,7 @@ public class ImageViewActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home)
-            finish();
+            supportFinishAfterTransition();
 
         return super.onOptionsItemSelected(item);
     }
