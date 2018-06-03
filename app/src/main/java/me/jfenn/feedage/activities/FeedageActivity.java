ce2e1f77581b2c02f039d41ba90bb50ae763a8e2
James Fenn <18jafenn90@gmail.com>
2018-06-03 15:57:26 -0400

    theme menu item icon colors

--- app/src/main/java/me/jfenn/feedage/activities/FeedageActivity.java
+++ app/src/main/java/me/jfenn/feedage/activities/FeedageActivity.java
@@ -1,8 +1,11 @@
 package me.jfenn.feedage.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import me.jfenn.feedage.Feedage;
 
@@ -19,5 +22,16 @@ public class FeedageActivity extends AppCompatActivity {
 
     public Feedage getFeedage() {
         return feedage;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        for (int i = 0; i < menu.size(); i++) {
+            Drawable icon = menu.getItem(i).getIcon();
+            if (icon != null)
+                DrawableCompat.setTint(icon, feedage.getTextColorSecondary());
+        }
+
+        return super.onPrepareOptionsMenu(menu);
     }
 }
